32a6437712eb87a520b47c899af62fa6acdc8cf2
Namesh Kher <nameshkher@Nameshs-MacBook-Pro.local>
2015-11-01 13:33:56 -0500

    Added MyStack Interface

---
+++ src/main/java/nkher/Interfaces/MyStack.java
@@ -0,0 +1,39 @@
+package nkher.Interfaces;
+
+public interface MyStack<T> {
+	
+	/***
+	 * Returns the size of the stack
+	 * 
+	 * @return
+	 */
+	int size();
+	
+	/***
+	 * Returns the top most element (Last inserted element) of the stack.
+	 * 
+	 * @return
+	 */
+	T peek();
+	
+	/***
+	 * Returns the top most element (Last inserted element) of the stack and removes it. 
+	 * 
+	 * @return
+	 */
+	T pop();
+	
+	/***
+	 * Pushes an element into the stack
+	 * 
+	 * @param element - data to be inserted
+	 */
+	void push(T element);
+	
+	/***
+	 * Returns true if the stack is empty else falses
+	 * 
+	 * @return
+	 */
+	boolean isEmpty();
+}
