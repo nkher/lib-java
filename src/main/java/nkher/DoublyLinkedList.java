13904dec171e16bcf2f19de5b1bfe581caf77d0b
Namesh Kher <nameshkher@Nameshs-MacBook-Pro.local>
2015-11-04 22:46:27 -0500

    Added functionality to doubly linked list

--- src/main/java/nkher/DoublyLinkedList.java
+++ src/main/java/nkher/DoublyLinkedList.java
@@ -1,6 +1,9 @@
 package nkher;
 
 import nkher.Interfaces.MyList;
+import nkher.exception.DataStructureEmptyException;
+import nkher.exception.DataStructureSmallerException;
+import nkher.exception.InvalidIndexException;
 
 /***
  * This class demonstrates the doubly linked list implementation.
@@ -20,6 +23,14 @@ public class DoublyLinkedList<T> implements MyList<T> {
 		this.size = 0;
 	}
 
+	/***
+	 * Represents the node of a doubly linked list. It contains a data element,
+	 * a pointer to the next node and a pointer to the previous node.
+	 * 
+	 * @author nameshkher
+	 *
+	 * @param <T>
+	 */
 	protected static class DoublyNode<T> {
 		T data;
 		DoublyNode<T> next;
@@ -61,7 +72,7 @@ public class DoublyLinkedList<T> implements MyList<T> {
 	 */
 	public T tail() {
 		if (head == null) return null;
-		return this.head.data;
+		return this.tail.data;
 	}
 	
 	/***
@@ -132,6 +143,40 @@ public class DoublyLinkedList<T> implements MyList<T> {
 	 * 
 	 */
 	public boolean remove(T element) {
+		if (head == null) {
+			throw new DataStructureEmptyException("Doubly Linked list is empty");
+		}
+		boolean found = false;
+		DoublyNode<T> itr = head;
+		while (itr != tail) {
+			if (itr.data.equals(element)) { // element found hence change the pointers
+				found = true;
+				size--; // decrement the size of the list
+				if (itr == head) {
+					head = head.next;
+					head.prev = null; // very important
+				}
+				else {
+					DoublyNode<T> prev = itr.prev;
+					prev.next = itr.next;
+					itr.next.prev = prev;
+				}
+				break;
+			}
+			if (itr == tail && itr.data.equals(element)) { // data is the last element
+				DoublyNode<T> last = tail;
+				tail = tail.prev;
+				tail.next = null;
+				last.prev = null;
+				found = true;
+				size--;
+			}
+			if (size == 1) tail = head;
+			if (found) {
+				return true;
+			}
+			return false;
+		}
 		return false;
 	}
 	
@@ -143,11 +188,103 @@ public class DoublyLinkedList<T> implements MyList<T> {
 	 * 
 	 */
 	public void removeAt(int index) {
-		
+		if (head == null) {
+			throw new DataStructureEmptyException("Doubly Linked list is empty");
+		}
+		else if (index > size - 1) {
+			throw new DataStructureSmallerException("Linked List Length is shorter than passed index.");
+		}
+		else if (index < 0) {
+			throw new InvalidIndexException("Cannot access negative index : " + index);
+		}
+		size--;
+		if (index == 0) { // deletion is at the end
+			DoublyNode<T> next = head.next;
+			if (next != null) {
+				next.prev = null;
+				head.next = null;
+				head = next;
+			}
+		}
+		else {
+			DoublyNode<T> itr = head;
+			int i = 0;
+			while (i < index) {
+				itr = itr.next;
+				i++;
+			}
+			DoublyNode<T> prev = itr.prev, next = itr.next;
+			if (null == itr.next) { // itr is the last node
+				prev.next = null;
+				itr.prev = null;
+				tail = prev; // update the tail
+				return;
+			}
+			// if it is not the last node
+			prev.next = next;
+			next.prev = prev;
+			itr.prev = null;
+			itr.next = null;
+		}
+		if (size == 1) tail = head;
 	}
 
+	/***
+	 * Returns the elements of the Doubly Linked List in a String for printing and viewing. 
+	 */
 	public String toString() {
-		return "";
+		if (isEmpty()) {
+			return new String("[ ]");
+		}
+		StringBuilder sb = new StringBuilder();
+		sb.append("[");
+		DoublyNode<T> itr = head;
+		while (itr != tail) {
+			sb.append(itr.data.toString() + ", ");
+			itr = itr.next;
+		}
+		sb.append(itr.data.toString());
+		sb.append("]");
+		return sb.toString();
 	}
 	
+	/***
+	 * Appends the passed doubly linked list at the tail of this doubly linked list.
+	 * This does not create new memory for the nodes that are being appended.
+	 * But it uses the existing nodes from the linked list which is to be appended.
+	 * 
+	 * @param linkedlist - a doubly linked list
+	 */
+	public void append(DoublyLinkedList<T> linkedlist) {
+		if (linkedlist.isEmpty() || linkedlist.head == null) { // nothing to append
+			return;
+		}
+		DoublyNode<T> itr = head;
+		while (itr != tail) {
+			itr = itr.next;
+		}
+		DoublyNode<T> itr2 = linkedlist.head;
+		while (itr2 != linkedlist.tail) {
+			insert(itr2);
+			itr2 = itr2.next;
+		}
+		insert(itr2); // insert the tail as well
+	}
+	
+	/***
+	 * Function to clone a doubly linked list.
+	 */
+	public DoublyLinkedList<T> clone() {
+		if (head == null) {
+			return new DoublyLinkedList<T>();
+		}
+		DoublyLinkedList<T> clone_list = new DoublyLinkedList<T>();
+		DoublyNode<T> itr = head;
+		while (itr != tail){
+			clone_list.insert(itr.data);
+			itr = itr.next;
+		}
+		clone_list.insert(itr);
+		return clone_list;
+	}
 }
