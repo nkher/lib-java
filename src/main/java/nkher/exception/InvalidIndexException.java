21a6c01eecb6ed0d1da929d17b650ef9f45ef6b9
Namesh Kher <nameshkher@Nameshs-MacBook-Pro.local>
2015-11-04 21:55:38 -0500

    Pushed InvalidIndexException and modified singly linked list

---
+++ src/main/java/nkher/exception/InvalidIndexException.java
@@ -0,0 +1,17 @@
+package nkher.exception;
+
+public class InvalidIndexException extends RuntimeException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -4122519035264237139L;
+	
+	public InvalidIndexException() {
+		super();
+	}
+
+	public InvalidIndexException(String message) {
+		super(message);
+	}
+}
