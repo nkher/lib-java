1278808a77f0054323cabac3e8a855b5f8b1d608
Namesh Kher <namesh.kher@gmail.com>
2015-11-04 21:49:58 -0500

    Added DataStructureSmallerException and modified SinglyLinkedList for correct exception type

---
+++ src/main/java/nkher/exception/DataStructureSmallerException.java
@@ -0,0 +1,18 @@
+package nkher.exception;
+
+public class DataStructureSmallerException extends RuntimeException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -2202350501720838737L; 
+	
+	public DataStructureSmallerException() {
+		super();
+	}
+	
+	public DataStructureSmallerException(String message) {
+		super(message);
+	}
+
+}
