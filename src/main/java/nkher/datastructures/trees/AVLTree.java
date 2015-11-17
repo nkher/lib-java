59d9107dff57484dd864a45345bf1854984527a2
Namesh Kher <namesh.kher@gmail.com>
2015-11-16 22:43:06 -0500

    Added partially working insert functionality to AVLTree

--- src/main/java/nkher/datastructures/trees/AVLTree.java
+++ src/main/java/nkher/datastructures/trees/AVLTree.java
@@ -8,7 +8,7 @@ public class AVLTree<K extends Comparable<K>, V> implements MyTree<K, V> {
 	private int size = 0;
 	private AVLNode<K, V> root;
 	
-	protected static class AVLNode<K extends Comparable<K>, V> {
+	public static class AVLNode<K extends Comparable<K>, V> {
 		private K key;
 		private V value;
 		private AVLNode<K, V> left;
@@ -133,7 +133,78 @@ public class AVLTree<K extends Comparable<K>, V> implements MyTree<K, V> {
 	}
 
 	public void insert(K k, V v) {
+		insert(new AVLNode<K, V>(k, v));
+	}
+	
+	/***
+	 * Algorithm to insert a node in a AVL tree.
+	 * Runtime - O(logN)
+	 * 
+	 * @param node - a {@code AVLNode} type node to be inserted
+	 */
+	public void insert(AVLNode<K, V> node) {
 		
+		if (node == null) {
+			throw new IllegalArgumentException("Cannot insert a null node");
+		}
+		
+		/* Performing a BST insertion */
+		size++;		
+		if (null == this.root) {
+			root = node;
+			return;
+		}
+		AVLNode<K, V> temp = this.root;
+		while (true) {
+			if (temp.key.compareTo(node.key) > 0) { // temp is greater then go left
+				if (temp.left != null) {
+					temp = temp.left;
+				}
+				else {
+					temp.left = node;
+					break;
+				}
+			}
+			else {
+				if (temp.right != null) {
+					temp = temp.right;
+				}
+				else {
+					temp.right = node;
+					break;
+				}
+			}
+		}
+		/* BST insertion done */
+		
+		/* Performing the balancing */
+		System.out.println("temp : " + temp.toString());
+		node.setHeight(Math.max(heightAt(temp.left), heightAt(temp.right)) + 1);
+		int balanceFactor = balanceAt(temp);
+		
+		/* Check all possible cases where balance can go wrong */
+		
+		// CASE 1 : LEFT LEFT CASE
+		if (balanceFactor > 1 && node.key.compareTo(temp.left.key) < 0) {
+			this.root = rightRotate(temp);
+		}
+		
+		// CASE 2 : LEFT RIGHT CASE
+		if (balanceFactor > 1 && node.key.compareTo(temp.left.key) > 0) {
+			temp.left = leftRotate(root.left);
+			this.root = rightRotate(root);
+		}
+		
+		// CASE 3 : RIGHT RIGHT CASE
+		if (balanceFactor < -1 && node.key.compareTo(temp.right.key) > 0) {
+			this.root = leftRotate(temp);
+		}
+		
+		// CASE 4 : RIGHT LEFT CASE
+		if (balanceFactor < -1 && node.key.compareTo(temp.right.key) < 0) {
+			root.right = rightRotate(root.right);
+			this.root = leftRotate(temp);
+		}		
 	}
 
 	public boolean remove(K k) {
@@ -146,6 +217,10 @@ public class AVLTree<K extends Comparable<K>, V> implements MyTree<K, V> {
 		}
 		return this.root.key;
 	}
+	
+	public AVLNode<K, V> root() {
+		return this.root;
+	}
 
 	public boolean isEmpty() {
 		return (this.size == 0);
@@ -157,11 +232,51 @@ public class AVLTree<K extends Comparable<K>, V> implements MyTree<K, V> {
 	 *******************************************************************************/
 	
 	private AVLNode<K, V> leftRotate(AVLNode<K, V> y) {
-		return null;
+		AVLNode<K, V> right = y.right;
+		AVLNode<K, V> rightsLChild = right.left;
+		
+		/** Rotation */
+		right.left = y;
+		y.right = rightsLChild.left;
+		
+		/** Fixing the heights */
+		right.height = Math.max(right.left.height, right.right.height) + 1;
+		y.height = Math.max(y.left.height, y.right.height) + 1;
+		
+		return right;
 	}
 	
 	private AVLNode<K, V> rightRotate(AVLNode<K, V> y) {
-		return null;
+		AVLNode<K, V> left = y.left; /* get the left child */
+		AVLNode<K, V> leftsRChild = left.right; /* get the right child of the left child */
+		
+		/** Rotation */
+		left.right = y;
+		y.left = leftsRChild;
+		
+		/** Fixing the heights */
+		left.height = Math.max(left.left.height, left.right.height) + 1;
+		y.height = Math.max(y.left.height, y.right.height) + 1;
+		
+		return left;
+	}
+	
+	/***
+	 * Function to get the balance at a particular subtree.
+	 * Helps for deciding on how to perform rotations.
+	 * Returns height of left subtree minus height of right subtree.
+	 * 
+	 * @param node
+	 * @return
+	 */
+	private int balanceAt(AVLNode<K, V> node) {
+		if (null == node) return 0;
+		return heightAt(node.left) - heightAt(node.right);
+	}
+	
+	private int heightAt(AVLNode<K, V> node) {
+		if (null == root) return 0;
+		return root.height;
 	}
 
 }
