5797939a9a463314c34dedb83e1b3ce8a6a96657
Namesh Kher <namesh.kher@gmail.com>
2015-11-18 23:54:47 -0500

    Added decompression function for non numeric strings

--- src/main/java/nkher/utils/StringUtility.java
+++ src/main/java/nkher/utils/StringUtility.java
@@ -12,6 +12,8 @@ public class StringUtility {
 	 * An efficient method for compressing a String using Run Length Encoding.
 	 * It optimizes for single char sequences by only appending the char and not the count (1). 
 	 * Internally uses a StringBuilder for forming the compression String.
+	 * These work good for Strings that have no numerics in them.
+	 * Example : The String should not contain [0 - 9]
 	 * 
 	 * @param str
 	 * @return
@@ -101,19 +103,80 @@ public class StringUtility {
 		if (n == 0) return str;
 		
 		StringBuilder sb = new StringBuilder();
+		int count = 0;
+		char prev = str.charAt(0);
 		
-		for (int i=n-1; i>=0 ; i--) {
-			System.out.println(str.charAt(i));
+		for (int i=1; i<n; i++) {			
+			if (Character.isDigit(str.charAt(i))) {					
+				while ((i <= n-1) && Character.isDigit(str.charAt(i))) {
+					count += (count * 10) + (str.charAt(i) - '0');
+					i++;
+				}
+				if (i == n-1) break;
+				i--;
+			}
+			else {
+				if (isNotDigit(str.charAt(i-1))) { // means prev seen digit was a number
+					sb.append(str.charAt(i-1));
+				}
+				else {					
+					while (count > 0) {
+						sb.append(prev);
+						count--;
+					}
+				}
+				prev = str.charAt(i);
+			}
+		}
+		while (count > 0) {
+			sb.append(prev);
+			count--;
+		}
+		if (count == 0) sb.append(prev);
+		return sb.toString();
+	}
+	
+	private static boolean isNotDigit(char ch) {
+		int n = (ch - '0');
+		return (n < 0 || n > 9);
+	}
+	
+	/***
+	 * Reverses the passed String and returns the reversed one.
+	 * 
+	 * @param str
+	 * @return
+	 */
+	public static String reverse(String str) {
+		
+		if (null == str) return null;
+		int n = str.length();
+		if (n == 0) return str;
+		
+		char[] arr = str.toCharArray();
+		int start = 0, end = n-1;
+		
+		while (start < end) {
+			char temp = arr[start];
+			arr[start] = arr[end];
+			arr[end] = temp;
+			start++;
+			end--;
 		}
 		
-		return null;
+		return new String(arr);
 	}
+
 	
 	public static void main(String args[]) {
 		
-		String str = "aabeedbbbesssff";
+		String str = "aabeedbbbesssffeh";
 		System.out.println(runLengthEncoding(str));
 		
-		System.out.println(decompress("a2be2db3es3f2"));
+		// System.out.println(decompress("a2be2db3es3f2").equals(str));
+		
+		System.out.println(decompress("a2be2db3es3f2eh").equals(str));
+		
+				
 	}
 }
