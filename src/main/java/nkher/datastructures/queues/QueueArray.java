465b3f79160f5480976141ace5093c22a9a86537
Namesh Kher <namesh.kher@gmail.com>
2015-12-07 18:48:26 -0500

    Added functionality to QueueArray.java

--- src/main/java/nkher/datastructures/queues/QueueArray.java
+++ src/main/java/nkher/datastructures/queues/QueueArray.java
@@ -3,7 +3,6 @@ package nkher.datastructures.queues;
 import java.util.Iterator;
 
 import nkher.Interfaces.MyQueue;
-import nkher.datastructures.lists.DynamicArray;
 import nkher.exception.DataStructureEmptyException;
 
 /***
@@ -20,10 +19,11 @@ import nkher.exception.DataStructureEmptyException;
 public class QueueArray<T> implements MyQueue<T>, Iterable<T> {
 	
 	public static final int DEFAULT_CAPACITY = 10;
+	public static final int k_SCALE_FACTOR = 3; // will increase storage by 3 times
 	
 	private int size;
 	private Object data[];
-	private int head = 0, tail = 0;
+	private int front = 0, rear = 0;
 	
 	public QueueArray() {
 		size = 0;
@@ -46,22 +46,70 @@ public class QueueArray<T> implements MyQueue<T>, Iterable<T> {
 		return size;
 	}
 
+	@SuppressWarnings("unchecked")
 	public T head() {
 		if (isEmpty()) {
 			throw new DataStructureEmptyException("Cannot get head of an empty queue");
 		}
-		return (T) data[head];
+		return (T) data[front];
+	}
+	
+	@SuppressWarnings("unchecked")
+	public T tail() {
+		if (isEmpty()) {
+			throw new DataStructureEmptyException("Cannot get tail of an empty queue");
+		}
+		return (T) data[rear];
 	}
 
 	public T dequeue() {
-		return null;
+		if (isEmpty()) {
+			throw new DataStructureEmptyException("Cannot remove from an empty queue");
+		}
+		@SuppressWarnings("unchecked")
+		T ret = (T) data[front];
+		front = (front+1) % data.length;
+		size--;
+		return ret;
 	}
 
 	/***
 	 * The resizing is handled by the dynamic array internally.
 	 */
 	public void enqueue(T element) {
-		
+		if (isFull()) {
+			resize();
+		}
+		size++;
+		data[rear] = element;
+		rear = (rear+1) % data.length;
+	}
+	
+	private boolean isFull() {
+		return (rear == data.length-1);		
+	}
+	
+	private void resize() {
+		int new_cap = size * k_SCALE_FACTOR;
+		Object new_data[] = new Object[new_cap];		
+		int i;
+		/* Fill the data */
+		int ind = 0;
+		if (front < rear) {
+			for (i=front; i<rear; i++) {
+				new_data[ind++] = data[i];
+			}
+		}
+		else {
+			for (i=front; i<data.length; i++) {
+				new_data[ind++] = data[i];
+			}
+			for (i=rear; i<front; i++) {
+				new_data[ind++] = data[i];
+			}
+		}
+		data = new_data;
+		front = 0;
 	}
 
 	public boolean isEmpty() {
@@ -69,29 +117,96 @@ public class QueueArray<T> implements MyQueue<T>, Iterable<T> {
 	}
 	
 	public String toString() {
-		return null;
+		if (isEmpty()) {
+			return "[ ]";
+		}
+		StringBuilder sb = new StringBuilder();
+		int i;
+		sb.append("[ ");
+		if (front < rear) {
+			for (i=front; i<rear-1; i++) {
+				sb.append(data[i] + ", ");
+			}
+			sb.append(data[i]);
+		}
+		else {
+			for (i=front; i<data.length; i++) {
+				sb.append(data[i] + ", ");
+			}
+			for (i=rear; i<front-1; i++) {
+				sb.append(data[i] + ", ");
+			}
+			sb.append(data[i]);
+		}
+		sb.append(" ]");
+		return sb.toString();
 	}
 	
 	public Object[] toArray() {
-		return null;
+		Object[] array;
+		if (isEmpty()) {
+			array = new Object[1];
+			return array;
+		}
+		array = new  Object[size];
+		int i, ind = 0;
+		if (front < rear) {
+			for (i=front;i<data.length; i++) {
+				array[ind++] = data[i];
+			}
+		}
+		else {
+			for (i=front; i<data.length; i++) {
+				array[ind++] = data[i];
+			}
+			for (i=rear; i<front; i++) {
+				array[ind++] = data[i];
+			}
+		}
+		return array;
 	}
 	
 	public void clear() {
-		
+		initQueueArray();
+		this.size = 0;
+		front = rear = 0;
 	}
 
 	public Iterator<T> iterator() {
-		return null;
+		return new QIterator();
 	}
 	
 	private class QIterator implements Iterator<T> {
-
+		
+		private int ind = 0;
+		private int currentFront = QueueArray.this.front;
+		private int currentRear = QueueArray.this.rear;
+		private Object[] data = QueueArray.this.data;
+		private T t;
+		
 		public boolean hasNext() {
-			return false;
+			return (ind != size);
 		}
 
+		@SuppressWarnings("unchecked")
 		public T next() {
-			return null;
+			if (!hasNext()) {
+				return null;
+			}
+			
+			if (currentFront < currentRear) {
+				t = (T) data[currentFront++];
+			}
+			else {
+				if (currentFront < data.length) {
+					t = (T) data[currentFront++];
+				}
+				else {
+					t = (T) data[currentRear++];
+				}
+			}
+			ind++;
+			return t;
 		}
 		
 	}
